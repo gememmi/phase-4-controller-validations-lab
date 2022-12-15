cc313471a41029a7f3d4277ca4cb5f0b0e4d802e
Emily <emelizadaniels@gmail.com>
2022-12-15 12:47:51 -0500

    commit tree 8b92142ea0f91c327add4aabd6f7f638f1ad111c

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,13 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+      render json: {errors: invalid.record.errors } , status: :unprocessable_entity
   end
+end
 
   private
 
@@ -20,4 +22,4 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
-end
+
